b3c65cf63f56f9c8c57fece63e10731a17022570
Ratson <github@ratson.name>
2018-12-22 20:25:24 +0800

    Add material-ui

--- src/main.js
+++ src/main.js
@@ -1,13 +1,22 @@
 // @flow
 import App from 'fusion-react';
 import Router from 'fusion-plugin-react-router';
-import Styletron from 'fusion-plugin-styletron-react';
+import MuiThemeProvider, {
+  MuiThemeProviderToken,
+  MuiThemeToken,
+} from 'fusion-plugin-material-ui';
+import {createMuiTheme} from '@material-ui/core/styles';
 
 import root from './root.js';
 
 export default () => {
   const app = new App(root);
-  app.register(Styletron);
   app.register(Router);
+  app.register(MuiThemeToken, createMuiTheme({
+    typography: {
+      useNextVariants: true,
+    },
+  }));
+  app.register(MuiThemeProviderToken, MuiThemeProvider);
   return app;
 };
