bdc662de739f031c689ee4979c0cbc5e8e5beef2
Ratson <github@ratson.name>
2018-12-22 21:47:47 +0800

    Add apollo

--- src/main.js
+++ src/main.js
@@ -1,22 +1,61 @@
 // @flow
-import App from 'fusion-react';
+import 'isomorphic-fetch';
+
 import Router from 'fusion-plugin-react-router';
-import MuiThemeProvider, {
-  MuiThemeProviderToken,
-  MuiThemeToken,
-} from 'fusion-plugin-material-ui';
+import MuiThemeProvider, { MuiThemeProviderToken, MuiThemeToken } from 'fusion-plugin-material-ui';
+import App, {ApolloClientToken} from 'fusion-apollo';
+import ApolloClientPlugin, { ApolloClientEndpointToken } from 'fusion-apollo-universal-client';
+import {GraphQLSchemaToken} from 'fusion-apollo';
+import {FetchToken} from 'fusion-tokens';
+import ApolloServer, { ApolloServerEndpointToken } from 'fusion-plugin-apollo-server';
 import {createMuiTheme} from '@material-ui/core/styles';
+import unfetch from 'unfetch';
+import {makeExecutableSchema} from 'graphql-tools';
 
 import root from './root.js';
 
+const typeDefs = `
+type Greeting {
+  message: String
+}
+
+type Query {
+  greeting: Greeting
+}
+`
+
+const resolvers = {
+  Query: {
+    greeting: () => ({
+      message: 'Hello World!',
+    }),
+  }
+}
+
 export default () => {
   const app = new App(root);
+  if (__BROWSER__) {
+    app.register(FetchToken, window.fetch);
+  }
+  if (__NODE__) {
+    app.register(FetchToken, unfetch);
+  }
   app.register(Router);
+
+  // apollo
+  __NODE__ && app.register(ApolloServer);
+  __NODE__ && app.register(ApolloServerEndpointToken, '/graphql');
+  app.register(ApolloClientEndpointToken, '/graphql');
+  app.register(ApolloClientToken, ApolloClientPlugin);
+  app.register(GraphQLSchemaToken, makeExecutableSchema({ typeDefs, resolvers }));
+
+  // material-ui
   app.register(MuiThemeToken, createMuiTheme({
     typography: {
       useNextVariants: true,
     },
   }));
   app.register(MuiThemeProviderToken, MuiThemeProvider);
+
   return app;
 };
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,13 +1,21 @@
 // @flow
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 
-const Home = () => (
+const Home = ({ data }) => (
   <div>
     <Button variant="contained" color="primary">
-      Hello World
+      {data.greeting ? data.greeting.message : 'Loading...'}
     </Button>
   </div>
 );
 
-export default Home;
+export default graphql(gql`
+  query GreetingQuery {
+    greeting {
+      message
+    }
+  }
+`)(Home);
